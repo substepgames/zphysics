88fab3c79221353e277b155587616b7102cc29a9
ivanjermakov <ivanjermakov1@gmail.com>
2026-09-12 01:02:32 +0200

    Fix cImport

--- src/zphysics.zig
+++ src/zphysics.zig
@@ -2,12 +2,12 @@ const builtin = @import("builtin");
 const std = @import("std");
 const assert = std.debug.assert;
 const options = @import("zphysics_options");
-const c = @cImport({
+pub const c = @cImport({
     if (options.use_double_precision) @cDefine("JPH_DOUBLE_PRECISION", "");
     if (options.enable_asserts) @cDefine("JPH_ENABLE_ASSERTS", "");
     if (options.enable_cross_platform_determinism) @cDefine("JPH_CROSS_PLATFORM_DETERMINISTIC", "");
     if (options.enable_debug_renderer) @cDefine("JPH_DEBUG_RENDERER", "");
-    @cInclude("JoltPhysicsC.h");
+    @cInclude("JoltC/JoltC.h");
 });
 
 pub const Real = c.JPC_Real;
